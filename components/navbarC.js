b1f4839d97ff39e2110f7780bf14db4e42bc4587
gk072745 <kumawatgovind222@gmail.com>
2022-08-25 23:15:20 +0530

    navbar and footer added

---
+++ components/navbarC.js
@@ -0,0 +1,27 @@
+function navfunc(){
+    return `<div id="navbar">
+    <div>
+     <h1>Booking.com</h1>
+        <div>
+            <h3>INR</h3>
+<img src="https://cf.bstatic.com/static/img/flags/new/48-squared/in/20aa535a5d3c505dd02fea275ed1a36c0fb1fe08.png" alt="">
+<i id="help" class="ri-question-line"></i>
+
+<a  href="https://join.booking.com/?lang=en-gb&utm_source=topbar&utm_medium=frontend&amp;label=gen173nr-1DCAEoggI46AdIM1gEaGyIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4AoiwkpgGwAIB0gIkNzA2YmQ5MmItOWEzYi00ZGE5LWE3MmQtNGMxZmVlYjBmZTY02AIE4AIB&amp;aid=304142">List your property</a>
+<a class="bcWhite" href="signIn.html">Register</a>
+<a class="bcWhite" href="signIn.html">Sign in</a>
+</div>
+    </div>
+    <div>
+ <a href=""><i class="ri-hotel-bed-line"></i>Stays</a>
+ <a href=""><i class="ri-flight-takeoff-line"></i>Flights</a>
+ <a href=""><i class="ri-global-line" ></i>Flight + Hotel</a>
+ <a href=""><i class="ri-car-line"></i>Car rentals</a>
+ <a href=""><i class="ri-baidu-line"></i>Attractions</a>
+ <a href=""><i class="ri-taxi-line"></i>Airport taxis</a>
+    </div>
+</div>`
+}
+
+
+export default navfunc
---
+++ navbar.js
@@ -0,0 +1,3 @@
+import navfunc from "./components/navbarC.js"
+document.getElementById("top").innerHTML=navfunc()
+
