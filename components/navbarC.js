fa7715f7148b80e64123df986eb20f1bb02a32dd
gk072745 <kumawatgovind222@gmail.com>
2022-08-26 22:56:42 +0530

    signup login complited and responsive improvment

--- components/navbarC.js
+++ components/navbarC.js
@@ -8,8 +8,12 @@ function navfunc(){
 <i id="help" class="ri-question-line"></i>
 
 <a  href="https://join.booking.com/?lang=en-gb&utm_source=topbar&utm_medium=frontend&amp;label=gen173nr-1DCAEoggI46AdIM1gEaGyIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4AoiwkpgGwAIB0gIkNzA2YmQ5MmItOWEzYi00ZGE5LWE3MmQtNGMxZmVlYjBmZTY02AIE4AIB&amp;aid=304142">List your property</a>
-<a class="bcWhite" href="signIn.html">Register</a>
-<a class="bcWhite" href="signIn.html">Sign in</a>
+
+<div id="dp">
+<a class="bcWhite"  href="signIn.html">Register</a>
+<a class="bcWhite"  href="signIn.html">Sign in</a>
+</div>
+
 </div>
     </div>
     <div>
