dac08a928681f9d983c597eaeae723d78e782126
gk072745 <kumawatgovind222@gmail.com>
2022-08-28 11:37:27 +0530

    edited

--- components/navbarC.js
+++ components/navbarC.js
@@ -14,12 +14,12 @@ ${dp()}
 </div>
     </div>
     <div id=""tabs>
- <a href="" id="tab1" onclick="tab1func()"><i class="ri-hotel-bed-line"></i>Stays</a>
- <a href="../carrental.html" id="tab2" onclick="tab2func()"><i class="ri-flight-takeoff-line"></i>Flights</a>
- <a href="" id="tab3" onclick="tab3func()"><i class="ri-global-line" ></i>Flight + Hotel</a>
- <a href="" id="tab4" onclick="tab4func()"><i class="ri-car-line"></i>Car rentals</a>
- <a href="" id="tab5" onclick="tab5func()"><i class="ri-baidu-line"></i>Attractions</a>
- <a href="" id="tab6" onclick="tab6func()"><i class="ri-taxi-line"></i>Airport taxis</a>
+ <a href="" id="tab1" class="active btn"><i class="ri-hotel-bed-line"></i>Stays</a>
+ <a href="" id="tab2"  class="btn"><i class="ri-flight-takeoff-line"></i>Flights</a>
+ <a href="" id="tab3"  class="btn"><i class="ri-global-line" ></i>Flight + Hotel</a>
+ <a href="" id="tab4"  class="btn"><i class="ri-car-line"></i>Car rentals</a>
+ <a href="" id="tab5"  class="btn"><i class="ri-baidu-line"></i>Attractions</a>
+ <a href="" id="tab6"  class="btn"><i class="ri-taxi-line"></i>Airport taxis</a>
     </div>
 </div>`
 }
@@ -37,7 +37,7 @@ function dp(){
 <img style="width:30px ;border:2px solid rgb(254,187,2)" src="https://cf.bstatic.com/static/img/identity/profile/b47cd0e05ec8b7831167f4f7593ead56402a6bb4.svg" alt="" />
         <div>
         <p style="line-height:0px"><b>Your account</b></p>
-        <p  style="line-height:5px;text-align:center;color:rgb(254,187,2);cursor: pointer;">Sign out</p>
+        <p  style="line-height:5px;text-align:center;color:rgb(254,187,2);cursor: pointer;" id="signout">Sign out</p>
         </div>
      
         </div>`
--- navbar.js
+++ navbar.js
@@ -1,12 +1,32 @@
 import navfunc from "./components/navbarC.js"
 document.getElementById("top").innerHTML=navfunc()
 
+let btn=document.getElementById("tabs")
+let btns=document.getElementsByClassName("btn")
 
-let tab3=document.getElementById("tab3").addEventListener("click",tab2func)
+document.getElementById("signout").addEventListener("click",signoutFunc)
 
-function tab2func(){
-   tab3.style.backgroundColor="red"
+// for(let i=0;i<btns.length;i++){
+//    btns[i].addEventListener("click",function(){
+//       let current=document.getElementsByClassName("active")
+//       for(let j=0;j<btns.length;j++){
+//          btns[j].classList.remove("active")
+//       }
+//       btns[i].classList.add("active")
+//    })
+// }
 
+
+
+// let links=document.querySelectorAll("#tabs a").
+// forEach(link=>{
+// if(link.href.includes(`${activePage}`)){
+// link.classList.add("active")
+// }
+// })
+
+
+function signoutFunc(){
+   localStorage.setItem("flag",false)
+   location. reload() 
 }
-
-
